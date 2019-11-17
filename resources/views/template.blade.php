af347eae15bee18e9a548c1c0b611c9753ade5b3
stuart Summers <summers2483@hotmail.com>
2019-11-17 16:23:33 +0100

    first commit

---
+++ resources/views/template.blade.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+        <title>Boxing App</title>
+    </head>
+    <body>
+      @include('_includes/nav/topnav')
+
+        <div class="container">
+            @yield('content')
+
+        </div>
+
+        {{-- Example of loops --}}
+            {{-- @if (Route::has('login'))
+                <div class="top-right links">
+                    @auth
+                        <a href="{{ url('/home') }}">Home</a>
+                    @else
+                        <a href="{{ route('login') }}">Login</a>
+
+                        @if (Route::has('register'))
+                            <a href="{{ route('register') }}">Register</a>
+                        @endif
+                    @endauth
+                </div>
+            @endif --}}
+    </body>
+</html>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,5 @@
+@extends('template')
+
+@section('content')
+    <p>My piece of content</p>
+@endsection
